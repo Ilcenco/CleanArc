628957adbdd32d712bf1546e8b27fcb7bd484c5a
Ilcenco <ilcenco.eugeniu@isa.utm.md>
2021-11-08 13:22:04 +0200

    Update JavaScript.js

--- Web/wwwroot/js/JavaScript.js
+++ Web/wwwroot/js/JavaScript.js
@@ -11,7 +11,6 @@ $(document).ready(function () {
             "ajax": {
                 "url": "/api/projects/getDataTable",
                 "type": "POST",
-                "dataType" : 'JSON',
             },
             "columns": [
                 { "data": "Name" },
